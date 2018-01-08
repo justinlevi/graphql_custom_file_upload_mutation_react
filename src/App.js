9dc0df2360e89a6db7fba7f57cf9a846804baec5
Justin Winter <justinlevi@gmail.com>
2018-01-08 13:46:45 -0500

    WIP
    # Conflicts:
    #	package.json
    #	src/App.js
    #	yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
+import UploadComponent from './components/UploadComponent';
 
-  render() {
-    return (
-      <div>
-        HELLO WORLD
-      </div>
-    );
+const submitFile = gql`
+  mutation fileUpload($input: FileUploadInput!) {
+    fileUpload(input: $input){
+      ...on File {
+        uri
+      }
+    }
   }
+`;
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(target){
+    this.props.mutate({
+        
+        variables: {
+          file: target.files, 
+          input : {
+            name: target.files[0].name
+          } 
+        }
+      }
+    ).then(response => {
+      console.log(response)
+    })
+  }
+
+  render = () => {
+    return ( 
+      <div>
+        <UploadComponent />
+        {/*<input
+          type="file"
+          required
+          multiple
+          onChange={({ target }) => {
+            target.validity.valid && this.handleChange(target)
+            }
+          }
+        />*/}
+        <button >UPLOAD THUMBNAILS</button>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default graphql(submitFile)(App);
